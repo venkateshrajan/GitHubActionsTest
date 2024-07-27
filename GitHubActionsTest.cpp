0306bf365d382d720e13e6d45ef8f9b1715fd5aa
Venkatesh Maharajan <venkatesh.xaviers@gmail.com>
2024-07-27 17:54:43 +0530

    initial cmake project

---
+++ GitHubActionsTest.cpp
@@ -0,0 +1,12 @@
+﻿// GitHubActionsTest.cpp : Defines the entry point for the application.
+//
+
+#include "GitHubActionsTest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
