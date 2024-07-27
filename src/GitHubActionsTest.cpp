61ee6e8876a7fdcc7f06121a46516f9929aead89
Venkatesh Maharajan <venkatesh.xaviers@gmail.com>
2024-07-27 18:36:50 +0530

    with gtest

---
+++ src/GitHubActionsTest.cpp
@@ -0,0 +1,11 @@
+﻿// GitHubActionsTest.cpp : Defines the entry point for the application.
+//
+
+#include "GitHubActionsTest.h"
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
